54f9b08d918786e44635fb0eda1abbff631b75d8
kost321 <kostyannn1996@gmail.com>
2022-05-28 17:04:12 +0300

    feat : add solution for HM-2 (in index.js)

--- lesson2/webpack/src/index.ts
+++ lesson2/webpack/src/index.ts
@@ -1,44 +1,48 @@
-interface Iusers {
-    name : string,
-    phone:string,
-    email:string,
-    animals?:string[],
-    cars?:string[],
-    hasChildren:boolean,
-    hasEducation:boolean
-}
+// interface Iusers {
+//     name : string,
+//     phone:string,
+//     email:string,
+//     animals?:string[],
+//     cars?:string[],
+//     hasChildren:boolean,
+//     hasEducation:boolean
+// }
 
 
-const user :Iusers[]  = [
-    {
-        name: "Harry Felton",
-        phone: "(09) 897 33 33",
-        email: "felton@gmail.com",
-        animals: ["cat"],
-        cars: ["bmw"],
-        hasChildren: false,
-        hasEducation: true
+// const user :Iusers[]  = [
+//     {
+//         name: "Harry Felton",
+//         phone: "(09) 897 33 33",
+//         email: "felton@gmail.com",
+//         animals: ["cat"],
+//         cars: ["bmw"],
+//         hasChildren: false,
+//         hasEducation: true
         
-    },
-    {
-        name: "May Sender",
-        phone: "(09) 117 33 33",
-        email: "sender22@gmail.com",
-        hasChildren: true,
-        hasEducation: true
-    },
-    {
-        name: "Henry Ford",
-        phone: "(09) 999 93 23",
-        email: "ford0@gmail.com",
-        cars: ["bmw", "audi"],
-        hasChildren: true,
-        hasEducation: false
-    }
-];
+//     },
+//     {
+//         name: "May Sender",
+//         phone: "(09) 117 33 33",
+//         email: "sender22@gmail.com",
+//         hasChildren: true,
+//         hasEducation: true
+//     },
+//     {
+//         name: "Henry Ford",
+//         phone: "(09) 999 93 23",
+//         email: "ford0@gmail.com",
+//         cars: ["bmw", "audi"],
+//         hasChildren: true,
+//         hasEducation: false
+//     }
+// ];
 
-user.forEach(id => {
-    console.log(id.name);
-});
+// user.forEach(id => {
+//     console.log(id.name);
+// });
 
 
+// user.forEach(id => {
+//      const sum = id.cars?.length; 
+//     console.log(sum);
+// });
